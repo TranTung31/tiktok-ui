d7083ef20d08ec0f8ec80533229ceb3ac9e6fcdd
Your Name <you@example.com>
2023-04-06 23:28:20 +0700

    Xây dựng UI phần sidebar #1

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -7,11 +7,13 @@ import Profile from '~/pages/Profile';
 import Upload from '~/pages/Upload';
 import Search from '~/pages/Search';
 import config from '~/config';
+import Live from '~/pages/Live/Live';
 
 // Routes không cần đăng nhập vẫn xem được
 const publicRoutes = [
     { path: config.routes.home, component: Home },
     { path: config.routes.following, component: Following },
+    { path: config.routes.live, component: Live },
     { path: config.routes.profile, component: Profile },
     { path: config.routes.upload, component: Upload, layout: HeaderOnly },
     { path: config.routes.search, component: Search, layout: null },
